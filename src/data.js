da220eb4f0072d816a295e346ad0df043a8c301f
Guilherme Ribeiro <guilherme_ribeiro52@icloud.com>
2024-12-28 16:17:34 -0300

    imagens dos trabalhos

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const works = [
+    {
+      id: 1,
+      title: "Brasfal",
+      image: "src/assets/images/brasfal.png",
+    },
+    {
+      id: 2,
+      title: "GR MAX PLUS",
+      image: "src/assets/images/compassVideo.png",
+    },
+    {
+      id: 3,
+      title: "Forever Homes",
+      image: "src/assets/images/ForeverHomes.png",
+    },
+    {
+      id: 4,
+      title: "Furniro",
+      image: "src/assets/images/furniro.png",
+    },
+  ];
+  
